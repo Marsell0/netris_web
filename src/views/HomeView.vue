be42fb7755ae5985569e3a22807b29cb6dd12c86
Marsell0 <m.marsox0d@mail.ru>
2023-06-24 16:41:35 +0500

    create graph

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,17 @@
       </div>
       <div class="main__sub">
         <div class="sub__count">
-          count
+          <ul class="count__list">
+            <li class="list__item" v-for="item in items" :key="item.message"> {{ item.message }}: {{ item.count }}</li>
+          </ul>
         </div>
-        <div class="sub__graph">
-          graph
+        <div class="sub__button">
+          <DragUpload></DragUpload>
         </div>
       </div>
+    </div>
+    <div class="graph">
+      <LineGraph></LineGraph>
     </div>
   </div>
 </template>
@@ -27,17 +32,12 @@ export default{
   },
   data: () => {
     return {
-      videoOptions: {
-        autoplay: true,
-        controls: true,
-        sources: [
-          {
-            src:
-              '/path/to/video.mp4',
-              type: 'video/mp4'
-          }
-        ]
-      }
+      items: [
+        {message: 'Трактор', count: '0'},
+        {message: 'Грузовая машина', count: '1'},
+        {message: 'Кран', count: '0'},
+        {message: 'Экскаватор', count: '2'},
+      ]
     }
   }
 }
@@ -50,28 +50,43 @@ export default{
   max-height: 4320px;
   margin: 0px auto;
   display: flex;
+  flex-direction: column;
 }
 .main {
-  background-color: rgb(110, 109, 109);
+  background-color: rgb(199, 198, 198);
+  border-radius: 25px;
   display: flex;
   flex-direction: row;
   flex: 1;
   padding: 1%;
+  margin-top: 4%;
 }
 .main__video {
-  background-color: aqua;
   flex: 1;
   width: 100%;
   height: 56.25%;
 }
 .main__sub {
-  background-color: green;
   flex: 0.5;
 }
 .sub__count {
-  background-color: blue;
+  background-color: rgb(233, 233, 233);
+  margin: 0 0 1% 1%;
+  border-top-right-radius: 10px;
 }
-.sub__graph {
-  background-color: yellow;
+.sub__button {
+  border: 1px solid black;
+  font-size: 25px;
+  margin: 0 0 0 1%;
 }
+.count__list{
+  list-style-type: none;
+  text-align: start;
+  margin-top: 0px;
+}
+.list__item{
+  font-size: 25px;
+  color: black;
+}
+
 </style>
