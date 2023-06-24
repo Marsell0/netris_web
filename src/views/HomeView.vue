e53d154b192a6efab0d35bcb0fbc5aee6f661281
Marsell0 <m.marsox0d@mail.ru>
2023-06-24 14:26:25 +0500

    create videoplayer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="_container">
+    <div class="main">
+      <div class="main__video">
+        <VideoPlayer></VideoPlayer>
+      </div>
+      <div class="main__sub">
+        <div class="sub__count">
+          count
+        </div>
+        <div class="sub__graph">
+          graph
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LineGraph from '@/components/LineGraph.vue'
+import DragUpload from '@/components/DragUpload.vue';
+import VideoPlayer from '@/components/VideoPlayer.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+export default{
+  components:{
+    LineGraph, DragUpload, VideoPlayer
+  },
+  data: () => {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        sources: [
+          {
+            src:
+              '/path/to/video.mp4',
+              type: 'video/mp4'
+          }
+        ]
+      }
+    }
   }
 }
+
 </script>
+
+<style scoped>
+._container {
+  max-width: 1533px;
+  max-height: 4320px;
+  margin: 0px auto;
+  display: flex;
+}
+.main {
+  background-color: rgb(110, 109, 109);
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  padding: 1%;
+}
+.main__video {
+  background-color: aqua;
+  flex: 1;
+  width: 100%;
+  height: 56.25%;
+}
+.main__sub {
+  background-color: green;
+  flex: 0.5;
+}
+.sub__count {
+  background-color: blue;
+}
+.sub__graph {
+  background-color: yellow;
+}
+</style>
